a12542ec529cbd055dfc0ab35cbf03dac8ada1a7
Agnus A. Horta <agnushorta@outlook.com>
2022-07-15 22:39:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Class IV
+
+*CONSTRUCTOR* is a epecial method on JavaScript class. When I call this JavaScript class later using **new** keyword, constructor will create one new blank JavaScript object and assign it properties and values based on the blue print inside.
---
+++ script.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', function(){
+    // cavas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+        constructor(game){
+            this.game = game;
+        }
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer{
+
+    }
+
+    class Background{
+
+    }
+
+    class UI {
+
+    }
+
+    class Game {
+
+    }
+});
